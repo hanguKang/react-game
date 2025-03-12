e05c8dbd1af3b4096cf1cae073ab52e39762c59e
redhangu <redhangu@gmail.com>
2025-03-12 18:22:26 +0900

    게임 스스로 작성중

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,39 @@ import Player from './components/Player'
 import Gameboard from './components/GameBoard'
 import Log from './components/Log'
 import './App.css';
-// gameTurns의 타입 정의
-type GameTurn = {
-  activeSquare: { row: number; col: number };
-  player: string;
-};
 
-type GameLog = [GameTurn, string[][]];
+type GameBoard = [
+  {activeSquare:{row:number, col:number}, player:string}
+]
+type GameBoardData = string[][]
+
+interface GameInfoProps {
+  gameboard: GameBoard
+  gameBoardData : GameBoardData
+}
 
 function App() {
-  const [gameTurns, setGameTurns] = useState<GameLog[]>([]); //게임 유저 턴 및 게임 완료 
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [activePlayer, setActivePlayer] = useState('X')
+  const [gameTurn, setGameTurn] = useState<GameInfoProps[]>([])
 
-  const handleSlectSqure = (rowIndex: number, colIdnex: number, newData:Array<[null | string, null | string, null | string]>)=>{
+  const handleGameSelected = (rowIndex:number, colIndex:number, curPlayer:string)=>{
+    setActivePlayer( prevActivePlayer => prevActivePlayer==='X' ? 'O':'X' );
+
+    console.log( rowIndex , colIndex ,curPlayer)
+
+    //if(gameTurn.length > 0 && rowIndex && colIndex && curPlayer){
+      setGameTurn( prevGameTurn => {
+        const currentPlayer = curPlayer;
+        return [
+          {activeSqure:{row:rowIndex,col:colIndex}, player:currentPlayer}, ...prevGameTurn
+        ]
+      })
+    //}else{}
+
     
-    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
-
-    setGameTurns(prevGameTurn => {  
-      let currentPlayer = 'X'; 
-      if(prevGameTurn.length>0 && currentPlayer === 'X') currentPlayer = 'O';
-      const updatedTurns: GameLog  = [
-        {activeSquare: {row:rowIndex, col:colIdnex}, player:currentPlayer },
-        newData.map((row) => row.map((cell) => cell || '')), //튜플로 선언된 2차원 배열을 string[][] 2차원 형식으로 만들어야 한다. 
-      ]
-      console.log([updatedTurns,...prevGameTurn]);
-      return [updatedTurns,...prevGameTurn]; 
-    });
-
   }
-  {console.log('activePlayer', activePlayer)}
+
+
   return (
     <>
       <ErrorBoundary>
@@ -46,9 +50,9 @@ function App() {
               <Player name={'player 1'} symbol="X" isActive={activePlayer === 'X'}/>
               <Player name={'player 2'} symbol="O" isActive={activePlayer === 'O'}/>
             </ol>
-            <Gameboard activePlayerSymbol ={activePlayer} onSelectSquare = {handleSlectSqure} turns={gameTurns}/>
+            
           </div>
-          <Log gameLog={gameTurns}/>
+          <Gameboard onSelected = {handleGameSelected} turn={gameTurn}/>
         </main>
     </>
   )
--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,19 +1,55 @@
 import React, {useState} from 'react'
+
+
 const initialGameBoard : Array<[null | string, null | string, null | string]> = [
     [null, null, null],
     [null, null, null],
     [null, null, null],
 ]
+type GameBoardInfo = [
+    activeSquare:{row:number, col:number},
+    player:string
+]
+type GamePlayData = string|null[][];
+interface GameBoardProps {
+    onSelected:()=>void
+    turns: GameBoardInfo & GamePlayData;
+}
 
-export default function Gameboard(){
+export default function Gameboard({onSelected, turns }:GameBoardProps){
+    const gameBoard : Array<[null | string, null | string, null | string]> = initialGameBoard.map((row) => [...row])
+    const player:string = 'X'
+    const handleBtnClick = (row:number, col:number, player:string)=>{
+        return function () {
+            //console.log(row, col);
 
+            onSelected(row, col, player)
 
-
+            for(const turn of turns){
+                const [gameBoardInfo] = turn; 
+                const {activeSquare, player} = gameBoardInfo;
+                const {row, col}= activeSquare;
+            }
+        }
+    }
 
     return(
         <ol id="game-board">
             { 
-                
+                gameBoard.map( (rowData, rowIndex) => {
+
+                    return <li key={rowIndex}>
+                        {
+                            rowData.map((colData, colIndex)=>{
+                                return (
+                                    <li key={rowIndex+'_'+colData}>
+                                        <button onClick={handleBtnClick(rowIndex, colIndex, player)}>{!gameBoard[rowIndex][colIndex]?'':}</button>
+                                    </li>
+                                )
+                            })
+                        }
+                    </li>
+                })
               
             }
         </ol>
